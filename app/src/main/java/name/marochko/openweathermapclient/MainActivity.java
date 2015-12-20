ea1b4d9b65dd9cc5e44193f2cdc5a0705a1907ba
Alexander Marochko <marochko@gmail.com>
2015-12-21 01:36:15 +0200

    Two activities and the cities list are ready

--- app/src/main/java/name/marochko/openweathermapclient/MainActivity.java
+++ app/src/main/java/name/marochko/openweathermapclient/MainActivity.java
@@ -2,15 +2,16 @@ package name.marochko.openweathermapclient;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int cityListId;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        textView = (TextView)findViewById(R.id.textView);
 
 /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -55,4 +57,16 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(data == null) return;
+
+        cityListId = data.getIntExtra("cityListId", 0);
+
+        textView.setText(cityListId + "");
+
+    }
+
 }
+
